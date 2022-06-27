f6d16a784999d73c9b62d634884cc198a982597e
AnuragSii <108224041+AnuragSii@users.noreply.github.com>
2022-06-28 00:16:15 +0200

    Today I edited, compiled and ran my first program

---
+++ Hello_world.c
@@ -0,0 +1,9 @@
+// stdio.h is a standard library that stores the printf and several other functions
+#include <stdio.h>
+//any C program starts with a 'main' function. 'main' is a keyword
+int main() {
+//printf displays the string inside the quotation
+printf("Hello World");
+// return 0;
+return 0;
+}
